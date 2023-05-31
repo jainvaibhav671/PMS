ce2cb7fe78d78b5a0780056341826890655b04f2
cse-vaibhav <jainvaibhav671@gmail.com>
2023-05-31 17:49:14 +0530

    Fixed type issue

--- src/app/[list_name]/page.tsx
+++ src/app/[list_name]/page.tsx
@@ -1,4 +1,10 @@
-export default function Page({ params }) {
+export default function Page({
+    params
+}: {
+    params: {
+        list_name: string
+    }
+}) {
 
     return <h1>Page: { params.list_name }</h1>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
 import TaskList from './components/TaskLists';
-import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 
 export default function Home() {
   const tasks = [1, 2, 3, 4];
   return (
     <main>
-      <Sidebar lists={tasks} />
       <div id="app">
         <Header />
         <TaskList tasks={tasks} />
